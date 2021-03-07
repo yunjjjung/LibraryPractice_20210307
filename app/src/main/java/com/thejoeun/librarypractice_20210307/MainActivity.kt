2dce39ce7998c4ab19a8ca1358ace965fdb6d735
최윤정 <jjjung1003@naver.com>
2021-03-07 16:12:44 +0900

    권한 방침 인터페이스 설정

--- app/src/main/java/com/thejoeun/librarypractice_20210307/MainActivity.kt
+++ app/src/main/java/com/thejoeun/librarypractice_20210307/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -23,10 +25,30 @@ class MainActivity : BaseActivity() {
             startActivity(myIntent)
         }
 
+//        전화걸기 버튼 눌리면 => 권한 있는지 확인하고 => 실제 전화 걸기
         callBtn.setOnClickListener {
-            val myUri= Uri.parse("tel: 010-2222-3333")
-            val myIntent = Intent(Intent.ACTION_CALL, myUri)
-            startActivity(myIntent)
+
+//            1. 권한이 있는지 / 거절 되었는지에 따른 행동 방침(Interface) 변수 생성
+//            권한이 있으면 => Uri /Action_call 등 실행
+//            권한 없으면 => 토스트로 연결 불가 안내
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+                    val myUri= Uri.parse("tel: 010-2222-3333")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext,"", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+
+//            2. 1에서 만든 행동 방침을 가지고 => 실제 권한 확인
+
+
         }
     }
 
