08bb568809dd83f03bd1798a166066df89d5b497
최윤정 <jjjung1003@naver.com>
2021-03-07 15:40:32 +0900

    photoview 라이브러리 활용 > 줌인 /줌 아웃 활용

--- app/src/main/java/com/thejoeun/librarypractice_20210307/MainActivity.kt
+++ app/src/main/java/com/thejoeun/librarypractice_20210307/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.thejoeun.librarypractice_20210307
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +15,12 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        이미지 뷰 클릭시 => 사진 보기 화면 이동.
+        profileImg.setOnClickListener {
+            val myIntent = Intent(mContext,ViewPhotoActivity::class.java)              //mContext 에서 출발해서 ViewPhotoActivity 로 갈꺼야
+            startActivity(myIntent)
+        }
     }
 
     override fun setValues() {
